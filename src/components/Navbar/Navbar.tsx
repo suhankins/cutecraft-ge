ee38e53ca00da69b5c5911a71e0365b05c3e642b
suhankins <punishedbernadetta@niuitmo.ru>
2023-07-17 18:55:32 +0400

    fixed navbar sometimes getting obsucred

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar({ children }: { children?: React.ReactNode }) {
     return (
-        <nav className="navbar sticky top-0 left-0 bg-base-300/100 text-neutral">
+        <nav className="navbar sticky top-0 left-0 z-40 bg-base-300/100 text-neutral">
             {children}
         </nav>
     );
