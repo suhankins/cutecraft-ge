ce24aa384dd8c0d779ce4a66cd75aa42abd54892
suhankins <punishedbernadetta@niuitmo.ru>
2023-07-22 15:08:07 +0400

    basic catalog page

---
+++ src/app/[lang]/catalog/page.tsx
@@ -0,0 +1,32 @@
+import { CategoryGrid } from '@/components/Category/CategoryGrid';
+import { MainBodyWidthContainer } from '@/components/MainBodyWidthContainer';
+import { Locale, i18n } from '@/lib/i18n-config';
+import { CategoryModel, SimpleCategory } from '@/models/Category';
+
+async function getCategories() {
+    const categories = (await CategoryModel.find()).map(
+        (category) => category.toObject() as SimpleCategory
+    );
+    return categories;
+}
+
+export const revalidate = false;
+export async function generateStaticParams() {
+    return i18n.locales.map((locale) => ({
+        lang: locale,
+    }));
+}
+
+export default async function Catalog({
+    params: { lang },
+}: {
+    params: { lang: Locale };
+}) {
+    const categories = await getCategories();
+
+    return (
+        <MainBodyWidthContainer>
+            <CategoryGrid categories={categories} lang={lang} />
+        </MainBodyWidthContainer>
+    );
+}
