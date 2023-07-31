397a0328719b8c430a774bb5e39a0a5daee20b9d
suhankins <punishedbernadetta@niuitmo.ru>
2023-07-31 19:47:11 +0400

    added className to mainbodywidthcontainer

--- src/components/MainBodyWidthContainer.tsx
+++ src/components/MainBodyWidthContainer.tsx
@@ -1,10 +1,14 @@
 export function MainBodyWidthContainer({
     children,
+    className,
 }: {
     children: React.ReactNode;
+    className?: string;
 }) {
     return (
-        <div className="mx-auto flex max-w-screen-lg flex-col gap-3 px-4">
+        <div
+            className={`mx-auto flex max-w-screen-lg flex-col gap-3 px-4 ${className}`}
+        >
             {children}
         </div>
     );
