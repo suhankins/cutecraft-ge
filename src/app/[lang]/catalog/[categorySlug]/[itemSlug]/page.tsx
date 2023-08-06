b4d8dd2a6caf97a428dc779a7550a1a914350014
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-06 20:00:50 +0400

    added item page

--- src/app/[lang]/catalog/[categorySlug]/[itemSlug]/page.tsx
+++ src/app/[lang]/catalog/[categorySlug]/[itemSlug]/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { Breadcrumbs } from '../../_components/Breadcrumbs';
 import { getCategory } from '../page';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 async function getCategoryAndItem(categorySlug: string, itemSlug: string) {
     const category = await getCategory(categorySlug);
@@ -24,7 +25,7 @@ export default async function Catalog({
     const dictionary = await getDictionary(lang);
     const [category, item] = await getCategoryAndItem(categorySlug, itemSlug);
 
-    if (!category || !item) {
+    if (!category || !item || !('price' in item)) {
         notFound();
     }
 
@@ -39,7 +40,30 @@ export default async function Catalog({
                     getLocalizedString(item.name, lang),
                 ]}
             />
-            Item page goes here
+            <main className="grid grid-cols-1 gap-8 sm:grid-cols-2">
+                <section>
+                    <img
+                        className="w-full rounded object-contain"
+                        src={item.images[0] || '/static/placeholder.png'}
+                        alt={getLocalizedString(item.name, lang)}
+                    />
+                </section>
+                <section className="flex flex-col gap-4">
+                    <h1 className="text-4xl font-bold">
+                        {getLocalizedString(item.name, lang)}
+                    </h1>
+                    <p>{getLocalizedString(item.description, lang)}</p>
+                    <div className="flex items-center gap-4">
+                        <span className="text-3xl font-bold">
+                            {item.price}&#8382;
+                        </span>
+                        <button className="btn-primary btn-square btn">
+                            {/* TODO: Add item to cart */}
+                            <ShoppingCartIcon className="h-8 w-6" />
+                        </button>
+                    </div>
+                </section>
+            </main>
         </MainBodyWidthContainer>
     );
 }
