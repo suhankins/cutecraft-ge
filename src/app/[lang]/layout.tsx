5fe2e47bbe2ac41b107e9d67cdf74b9ad77566b4
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-13 18:06:13 +0400

    lang is set properly on html now

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -30,62 +30,75 @@ export default async function Layout({ children, params: { lang } }: Params) {
     const dictionary = await getDictionary(lang);
 
     return (
-        <CartProvider>
-            <Navbar>
-                <div className="dropdown sm:hidden">
-                    <label tabIndex={0} className="btn-ghost btn m-1">
-                        <Bars3Icon className="h-6 w-6" />
-                    </label>
-                    <ul
-                        tabIndex={0}
-                        className="dropdown-content menu rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
-                    >
-                        <li>
-                            <Link href="/">{dictionary.links.homeLink}</Link>
-                        </li>
-                        <li>
-                            <Link href="/catalog">
-                                {dictionary.links.catalogLink}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/about">
-                                {dictionary.links.aboutLink}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/contact">
-                                {dictionary.links.contactLink}
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <Link href="/" className="btn-ghost btn text-xl normal-case">
-                    {dictionary.companyName}
-                </Link>
-                <ul className="menu menu-horizontal hidden flex-grow items-center justify-center sm:inline-flex">
-                    <li>
-                        <Link href="/">{dictionary.links.homeLink}</Link>
-                    </li>
-                    <li>
-                        <Link href="/catalog">
-                            {dictionary.links.catalogLink}
+        <html lang={lang}>
+            <body>
+                <CartProvider>
+                    <Navbar>
+                        <div className="dropdown sm:hidden">
+                            <label tabIndex={0} className="btn-ghost btn m-1">
+                                <Bars3Icon className="h-6 w-6" />
+                            </label>
+                            <ul
+                                tabIndex={0}
+                                className="dropdown-content menu rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
+                            >
+                                <li>
+                                    <Link href="/">
+                                        {dictionary.links.homeLink}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/catalog">
+                                        {dictionary.links.catalogLink}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/about">
+                                        {dictionary.links.aboutLink}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact">
+                                        {dictionary.links.contactLink}
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <Link
+                            href="/"
+                            className="btn-ghost btn text-xl normal-case"
+                        >
+                            {dictionary.companyName}
                         </Link>
-                    </li>
-                    <li>
-                        <Link href="/about">{dictionary.links.aboutLink}</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">
-                            {dictionary.links.contactLink}
-                        </Link>
-                    </li>
-                </ul>
-                <LanguagePickerViewer selectedLang={lang} />
-                <CartDisplay />
-            </Navbar>
-            <main className="h-full w-full flex-grow">{children}</main>
-            <Footer dictionary={dictionary.links} />
-        </CartProvider>
+                        <ul className="menu menu-horizontal hidden flex-grow items-center justify-center sm:inline-flex">
+                            <li>
+                                <Link href="/">
+                                    {dictionary.links.homeLink}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/catalog">
+                                    {dictionary.links.catalogLink}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/about">
+                                    {dictionary.links.aboutLink}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/contact">
+                                    {dictionary.links.contactLink}
+                                </Link>
+                            </li>
+                        </ul>
+                        <LanguagePickerViewer selectedLang={lang} />
+                        <CartDisplay />
+                    </Navbar>
+                    <main className="h-full w-full flex-grow">{children}</main>
+                    <Footer dictionary={dictionary.links} />
+                </CartProvider>
+            </body>
+        </html>
     );
 }
---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,7 @@
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
