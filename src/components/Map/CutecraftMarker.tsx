9ec34806aaa9f03401ef46d97b6bd4f11e762adb
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-13 22:03:04 +0400

    changed marker colors

--- src/components/Map/CutecraftMarker.tsx
+++ src/components/Map/CutecraftMarker.tsx
@@ -9,10 +9,10 @@ export function CutecraftMarker({
 }) {
     return (
         <div
-            className="tooltip-open tooltip tooltip-right absolute -translate-x-1/2 -translate-y-full text-primary"
+            className="tooltip-open tooltip-primary tooltip tooltip-right absolute -translate-x-1/2 -translate-y-full"
             data-tip={text}
         >
-            <MapPinIcon className=" h-8 w-8" />
+            <MapPinIcon className="h-8 w-8 text-primary" />
         </div>
     );
 }
