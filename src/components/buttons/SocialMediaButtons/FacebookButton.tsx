7f4d493af1db2c1242303d91210b054aef3a374b
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-23 17:15:59 +0400

    added real facebook link

--- src/components/buttons/SocialMediaButtons/FacebookButton.tsx
+++ src/components/buttons/SocialMediaButtons/FacebookButton.tsx
@@ -2,11 +2,10 @@ export function FacebookButton() {
     return (
         <a
             className="btn-facebook btn flex w-auto flex-nowrap gap-2 px-8"
-            href="https://www.instagram.com/cute_craft.ge/" // TODO: Find them on facebook
+            href="https://www.facebook.com/groups/2115643001964114/?ref=share"
             target="_blank"
         >
-            <span>Facebook</span>{' '}
-            {/* Facebook doesn't offer nice look @ usernames */}
+            <span>Facebook</span>
             <img
                 alt="Facebook logo"
                 src="/static/facebook.png"
