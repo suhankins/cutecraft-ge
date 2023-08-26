4dbcf6b51590b16d76d9e1ed87f2bd2d75b7888c
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-26 17:10:35 +0400

    comebacklater middleware now redirects from comebacklater

--- src/middleware/comeBackLaterMiddleware.ts
+++ src/middleware/comeBackLaterMiddleware.ts
@@ -2,26 +2,40 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const redirectLocation = 'comebacklater';
 
-function checkIfAlreadyOnComeBackLater(pathname: string) {
+/**
+ * Checks if user is on /comeBackLater.
+ */
+export function checkIfAlreadyOnComeBackLater(pathname: string) {
     const splitPathname = pathname.split('/');
     return (
         splitPathname.at(-1) === redirectLocation &&
+        splitPathname.length <= 3 &&
         (splitPathname.at(1) === redirectLocation ||
             splitPathname.at(2) === redirectLocation)
     );
 }
 
 /**
- * Checks for COME_BACK_LATER env variable and redirects to /come-back-later if true
+ * Checks for COME_BACK_LATER env variable and redirects to /come-back-later if true.
+ *
+ * If the user is already on /come-back-later and COME_BACK_LATER is not set, it will redirect to /.
+ *
+ * Else just returns null.
  */
 export function comeBackLaterMiddleware(request: NextRequest) {
-    if (
-        process.env.COME_BACK_LATER &&
-        !checkIfAlreadyOnComeBackLater(request.nextUrl.pathname)
-    ) {
+    if (checkIfAlreadyOnComeBackLater(request.nextUrl.pathname)) {
+        if (process.env.COME_BACK_LATER !== 'true') {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+
+        return null;
+    }
+
+    if (process.env.COME_BACK_LATER === 'true') {
         return NextResponse.redirect(
             new URL('/' + redirectLocation, request.url)
         );
     }
+
     return null;
 }
