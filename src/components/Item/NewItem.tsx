a223a3847f4506852eaf3821044313f77ac7271e
suhankins <punishedbernadetta@niuitmo.ru>
2023-10-04 19:46:24 +0400

    removed raw svg in code

--- src/components/Item/NewItem.tsx
+++ src/components/Item/NewItem.tsx
@@ -1,4 +1,5 @@
 import { newItem } from '@/utils/client/new/newItem';
+import { PlusIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 import { useSWRConfig } from 'swr';
 
@@ -30,20 +31,7 @@ export function NewItem({
             disabled={loading}
             onClick={handleNewItemClick}
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                />
-            </svg>
+            <PlusIcon className="h-6 w-6" />
             New item
         </button>
     );
