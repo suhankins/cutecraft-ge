16c3558afd611989e1780f7131c741b79005c46a
suhankins <punishedbernadetta@niuitmo.ru>
2023-10-15 19:46:31 +0400

    improved checkout page

--- src/app/[lang]/(withNavbar)/checkout/_components/CartViewer.tsx
+++ src/app/[lang]/(withNavbar)/checkout/_components/CartViewer.tsx
@@ -21,68 +21,70 @@ export function CartViewer({
     const cart = useContext(CartContentsContext);
 
     return (
-        <table className="w-full border-collapse">
-            <tbody>
-                {cart.map((item, index) => (
-                    <tr key={index} className="border-b-2">
-                        <td className="w-full py-3">
-                            <p className="text-lg font-bold">
-                                {item.name[lang]}
+        <div className="w-full">
+            <table className="w-full border-collapse">
+                <tbody>
+                    {cart.map((item, index) => (
+                        <tr key={index} className="border-b-2">
+                            <td className="w-full py-3">
+                                <p className="text-lg font-bold">
+                                    {item.name[lang]}
+                                </p>
+                            </td>
+                            <td className="px-2">
+                                <button
+                                    className="btn-primary btn-circle btn"
+                                    onClick={() => removeFromCart(item)}
+                                >
+                                    {item.quantity > 1 ? (
+                                        <MinusIcon className="h-6 w-6" />
+                                    ) : (
+                                        <TrashIcon className="h-6 w-6" />
+                                    )}
+                                </button>
+                            </td>
+                            <td>
+                                <p className="w-full px-2 text-center text-3xl font-bold">
+                                    {item.quantity}
+                                </p>
+                            </td>
+                            <td className="px-2">
+                                <button
+                                    className="btn-primary btn-circle btn"
+                                    onClick={() => addToCart(item)}
+                                >
+                                    <PlusIcon className="h-6 w-6" />
+                                </button>
+                            </td>
+                            <td className="px-4">
+                                <p className="text-right text-3xl font-bold">
+                                    {item.price * (item.quantity ?? 1)}&#8382;
+                                </p>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td />
+                        <td className="w-full py-3" colSpan={3}>
+                            <p className="text-right text-2xl font-bold">
+                                {dictionary.total}
                             </p>
-                        </td>
-                        <td className="px-2">
-                            <button
-                                className="btn-primary btn-square btn"
-                                onClick={() => removeFromCart(item)}
-                            >
-                                {item.quantity && item.quantity > 1 ? (
-                                    <MinusIcon className="h-6 w-6" />
-                                ) : (
-                                    <TrashIcon className="h-6 w-6" />
-                                )}
-                            </button>
-                        </td>
-                        <td>
-                            <p className="w-full px-2 text-center text-3xl font-bold">
-                                {item.quantity ?? 1}
-                            </p>
-                        </td>
-                        <td className="px-2">
-                            <button
-                                className="btn-primary btn-square btn"
-                                onClick={() => addToCart(item)}
-                            >
-                                <PlusIcon className="h-6 w-6" />
-                            </button>
                         </td>
                         <td className="px-4">
-                            <p className="text-right text-3xl font-bold">
-                                {item.price * (item.quantity ?? 1)}&#8382;
+                            <p className="text-3xl font-bold">
+                                {cart.reduce(
+                                    (acc, item) =>
+                                        acc + item.price * item.quantity,
+                                    0
+                                )}
+                                &#8382;
                             </p>
                         </td>
                     </tr>
-                ))}
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td />
-                    <td className="w-full py-3" colSpan={3}>
-                        <p className="text-right text-2xl font-bold">
-                            {dictionary.total}
-                        </p>
-                    </td>
-                    <td className="px-4">
-                        <p className="text-3xl font-bold">
-                            {cart.reduce(
-                                (acc, item) =>
-                                    acc + item.price * (item.quantity ?? 1),
-                                0
-                            )}
-                            &#8382;
-                        </p>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+                </tfoot>
+            </table>
+        </div>
     );
 }
