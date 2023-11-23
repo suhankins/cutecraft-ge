a7bbae4fa382a74f4343077ee4beea36ad9ab78b
suhankins <punishedbernadetta@niuitmo.ru>
2023-11-23 18:09:51 +0400

    liited navbar width

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+import { MainBodyWidthContainer } from '../MainBodyWidthContainer';
+
 export function Navbar({ children }: { children?: React.ReactNode }) {
     return (
         <nav className="navbar sticky top-0 left-0 z-40 bg-base-300/100 text-neutral">
-            {children}
+            <MainBodyWidthContainer className="w-full">
+                <div className="navbar min-h-0 p-0">{children}</div>
+            </MainBodyWidthContainer>
         </nav>
     );
 }
