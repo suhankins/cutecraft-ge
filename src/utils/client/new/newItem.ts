ca810f49a665521814a6726a449fca9905665f29
suhankins <punishedbernadetta@niuitmo.ru>
2024-02-05 16:03:17 +0400

    fixed same new item slugs

--- src/utils/client/new/newItem.ts
+++ src/utils/client/new/newItem.ts
@@ -2,7 +2,7 @@ export async function newItem(categoryId: string) {
     const result = await fetch(`/api/category/${categoryId}/items/`, {
         method: 'POST',
         body: JSON.stringify({
-            name: { en: 'New Item' },
+            name: { en: `New Item ${Math.round(Math.random() * 10000)}` },
         }),
     });
     if (result.ok) console.log('New item added successfully');
