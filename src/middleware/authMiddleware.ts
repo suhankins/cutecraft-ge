43379862ab5bac82ae3861d2b48dfe56dac29ca2
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-16 13:15:43 +0400

    removed login requirement in dev mode

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -4,6 +4,7 @@ import { NextRequest } from 'next/server';
  * @returns {boolean} true if request doesn't require authentication, false otherwise
  */
 export function authMiddleware(request: NextRequest): boolean {
+    if (process.env.NODE_ENV === 'development') return true;
     const url = request.nextUrl.clone();
     const pathname = url.pathname.split('/');
     pathname.shift(); // remove the first empty string
