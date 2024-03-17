09a64b6f6d7e842ffe7ac51c66d9722b41312e93
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-17 14:01:10 +0400

    reloading admin panel now correctly sets back language

--- src/app/admin/_components/LanguagePickerEditor.tsx
+++ src/app/admin/_components/LanguagePickerEditor.tsx
@@ -1,4 +1,5 @@
 import { Locale, i18n } from '@/lib/i18n-config';
+import { useCallback } from 'react';
 
 export function LanguagePickerEditor({
     className = '',
@@ -10,8 +11,18 @@ export function LanguagePickerEditor({
     setLang: (lang: Locale) => void;
 }) {
     const languages = i18n.locales;
+    const selectRef = useCallback(
+        (selectElement: HTMLSelectElement) => {
+            if (!selectElement) {
+                return;
+            }
+            setLang(selectElement.value as Locale);
+        },
+        [setLang]
+    );
     return (
         <select
+            ref={selectRef}
             className={`select uppercase ${className}`}
             onChange={(event) => {
                 setLang(event.target.value as Locale);
